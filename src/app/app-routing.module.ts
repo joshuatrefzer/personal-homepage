1d4fde1dd1d6d820e842118ae6a8080198f0e12f
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-05-09 22:29:00 +0200

    website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './menu/imprint/imprint.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: ''  , component: SlideshowComponent},
+  { path: 'imprint' , component: ImprintComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
