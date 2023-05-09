8c0da46421128e59eab72e76d741592d7573b0b4
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-05-09 14:28:48 +0200

    added ts for imgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'pc-img.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage =false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+
+    }, 5000);
+
+  }
+
+
+
+
 }
